ff5bfca798c741a2ec709458ebcb58b88cf0586e
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2026-09-18 10:10:04 +0700

    test: bump tooling parser corpus count to 168
    Two new examples (csv_cleaner.lox, config_validator.lox) grow the corpus: 114 examples + 2 bootstrap + 52 probes.

--- test/test_tooling_parser.cpp
+++ test/test_tooling_parser.cpp
@@ -534,9 +534,9 @@ std::size_t measureProgramDepth(const Program& prog) {
 
 TEST(ToolingParserCorpus, ParsesEveryFileWithoutCrash) {
     const std::vector<fs::path> files = corpusFiles();
-    // examples/*.lox (111) + bootstrap/*.lox (2) + translation-probes/*.lox
+    // examples/*.lox (114) + bootstrap/*.lox (2) + translation-probes/*.lox
     // (52)
-    ASSERT_EQ(files.size(), 166U);
+    ASSERT_EQ(files.size(), 168U);
 
     std::size_t totalNodes = 0;
     for (const auto& file : files) {
